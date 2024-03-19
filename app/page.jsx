6292734d23f23f709dd39ca7ca40e914ca4185b8
apo-ll <marsappollo3@gmaill.com>
2024-03-19 16:09:36 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import { createClient } from "@supabase/supabase-js";
+import { BlurImage } from "@/components/blur-image";
+
+async function images() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+  );
+
+  const { data } = await supabaseAdmin.from("images").select("*").order("id");
+  return data;
+}
+
+export default async function Home() {
+  const image = await images();
+  return (
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {image &&
+          image.map((imager) => <BlurImage key={imager.id} image={imager} />)}
+      </div>
+    </div>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kiiufwkytrrgjpokwexf.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
