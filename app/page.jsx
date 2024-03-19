7695da2d31115d2616ed79bc5fcb4bb0b4c89670
apo-ll <marsappollo3@gmaill.com>
2024-03-19 16:50:25 +0300

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { BlurImage } from "@/components/blur-image";
-
-async function images() {
-  const supabaseAdmin = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
-  );
-
-  const { data } = await supabaseAdmin.from("images").select("*").order("id");
-  return data;
-}
+import { redis } from "@/lib/redis";
 
 export default async function Home() {
   const image = await images();
@@ -21,4 +12,20 @@ export default async function Home() {
       </div>
     </div>
   );
+}
+
+async function images() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
+  );
+
+  const { data } = await supabaseAdmin.from("images").select("*").order("id");
+  const cachedData = await redis.get("hello");
+
+  if (cachedData) {
+    return cachedData;
+  }
+  await redis.set("hello", data);
+  return data;
 }
